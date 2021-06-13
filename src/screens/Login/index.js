3933c60060f63c3a280b1b4681e36ca8f345263f
magcodes <70335377+magcodes@users.noreply.github.com>
2021-06-13 11:08:22 +0000

    Login Screen UI

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,38 +1,11 @@
 import React from 'react';
-
-import {Text, TextInput, View} from 'react-native';
-import Container from '../../components/common/Container';
-import CustomButton from '../../components/common/CustomButton';
-import Input from '../../components/common/Input';
+import LoginComponent from '../../components/Login';
 
 const Login = () => {
   // const [value, onChangeText] = React.useState('');
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
-  return (
-    <Container>
-      <Input
-        label="Username"
-        onChangeText={text => setUsername(text)}
-        value={username}
-        iconPosition="right"
-        // error={'This field is required'}
-      />
-
-      <Input
-        label="Password"
-        onChangeText={setPassword}
-        value={password}
-        icon={<Text>HIDE</Text>}
-        iconPosition="right"
-      />
-
-      <CustomButton secondary title="Submit" loading={true} disabled={true} />
-      <CustomButton secondary loading title="Click Me" />
-      <CustomButton primary title="Submit" loading={true} disabled={true} />
-      <CustomButton danger title="Submit" />
-    </Container>
-  );
+  return <LoginComponent />;
 };
 
 export default Login;
